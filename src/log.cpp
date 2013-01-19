f1d6b5bb6a1e74d7063ecde76ef3c5e217414c0a
Bryan Conrad <conrad.bryan.k@gmail.com>
2013-01-19 12:54:43 -0800

    add Log::p function

---
+++ src/log.cpp
@@ -0,0 +1,17 @@
+#include "log.h"
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+namespace Log {
+   void p(const char* format, ...)
+   {
+      string s(format);
+      s += "\n";
+      va_list argptr;
+      va_start(argptr, format);
+      vfprintf(stdout, s.c_str(), argptr);
+      va_end(argptr);
+   }
+}
---
+++ src/log.h
@@ -0,0 +1,10 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <cstdarg>
+
+namespace Log {
+   void p(const char* format, ...);
+}
+
+#endif // LOG_H
