711cb430130c20e3a021caef36df8c93568e3182
Bryan Conrad <conrad.bryan.k@gmail.com>
2013-01-22 05:55:58 -0800

    fix tests

--- src/client/video.h
+++ src/client/video.h
@@ -11,7 +11,7 @@ class Entity;
 class Video
 {
    public:
-      Video(irr::IEventReceiver* eventReceiver);
+      explicit Video(irr::IEventReceiver* eventReceiver = NULL);
       virtual ~Video();
       bool run();
       void addEntity(Entity* entity);
